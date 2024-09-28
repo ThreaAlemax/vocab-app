d76e7172e1911428d3e4798a6ea4189729f728e8
viperdragon <alex.sarro@yahoo.com>
2024-09-28 12:50:58 -0400

    feat: add training title name

--- src/components/pages/Practice.js
+++ src/components/pages/Practice.js
@@ -160,10 +160,10 @@ function Practice() {
 
   return (
     <div className="p-4 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold mb-6">Practice</h1>
       {training ? (
         <div>
-          <ChatBox alerts={chatBoxAlerts} messages={chatBoxMessages} />
+          <h1 className="text-2xl font-bold mb-6">Practice {training.name}</h1>
+          <ChatBox alerts={chatBoxAlerts} messages={chatBoxMessages}/>
           <input
             type="text"
             value={userAnswer}
